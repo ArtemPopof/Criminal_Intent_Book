48e19ab19243501f178963e5342a72b0287bed2a
ArtemPopof <Deviling42@gmail.com>
2016-09-24 13:03:11 +0300

    Criminal Fragment added

--- java/com/brizzgames/criminalintent/CriminalFragment.java
+++ java/com/brizzgames/criminalintent/CriminalFragment.java
@@ -2,9 +2,12 @@ package com.brizzgames.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Controller class for Crime model
@@ -17,6 +20,7 @@ import android.view.ViewGroup;
 public class CriminalFragment extends Fragment {
 
     private Crime crime;
+    private EditText titleField;
 
     @Override
     public void onCreate(Bundle onSavedInstanceState) {
@@ -31,6 +35,33 @@ public class CriminalFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View  v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        titleField = (EditText) v.findViewById(R.id.crime_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+
+                crime.setTitle(c.toString());
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+
+                // empty space for further development
+
+            }
+
+            public void afterTextChanged(Editable c) {
+
+                // too
+
+            }
+
+        });
+
+
         return v;
 
     }
